1f08450e74afd0eb5f0816eccaf1ca2404903ca4
InSync <insyncwithfoo@gmail.com>
2024-05-01 19:14:34 +0000

    Fix the tests

--- src/tightwrap/__init__.py
+++ src/tightwrap/__init__.py
@@ -1,8 +1,10 @@
 import sys
 
 from functools import wraps as functools_wraps
-from inspect import Signature, signature
-from typing import Any, Callable, TypeVar
+from inspect import Signature
+from typing import Any, Callable, TypeVar, cast
+
+from ._backported import get_annotations, eval_if_necessary
 
 if sys.version_info >= (3, 10):
     from typing import ParamSpec
@@ -14,20 +16,34 @@ T = TypeVar("T")
 R = TypeVar("R")
 
 
+def _get_resolved_signature(fn: Callable[..., Any]) -> Signature:
+    signature = Signature.from_callable(fn)
+    evaluated_annotations, fn_globals, fn_locals = get_annotations(fn)
+    
+    for name, parameter in signature.parameters.items():
+        setattr(parameter, '_annotation', evaluated_annotations[name])
+    
+    new_return_annotation = eval_if_necessary(signature.return_annotation, fn_globals, fn_locals)
+    setattr(signature, '_return_annotation', new_return_annotation)
+    
+    return signature
+
+
 def wraps(wrapped: Callable[P, Any]) -> Callable[[Callable[..., R]], Callable[P, R]]:
     """Apply `functools.wraps`"""
 
-    def wrapper(fn):
-        wrapper_return = signature(fn, eval_str=True).return_annotation
+    def wrapper(fn: Callable[..., R]) -> Callable[P, R]:
+        wrapper_return = _get_resolved_signature(fn).return_annotation
         res = functools_wraps(wrapped)(fn)
 
-        orig_sig = signature(wrapped, eval_str=True)
+        orig_sig = _get_resolved_signature(wrapped)
+        
         if orig_sig.return_annotation != wrapper_return:
             # We do a little rewriting.
             new_sig = Signature(None, return_annotation=wrapper_return)
-            new_sig._parameters = orig_sig._parameters
-            res.__signature__ = new_sig
+            setattr(new_sig, '_parameters', orig_sig.parameters)
+            setattr(res, '__signature__', new_sig)
 
-        return res
+        return cast(Callable[P, R], res)
 
     return wrapper
---
+++ src/tightwrap/_backported.py
@@ -0,0 +1,85 @@
+# type: ignore
+
+import sys
+from types import GetSetDescriptorType, ModuleType
+from typing import Callable, Any, Mapping, Tuple
+import functools
+
+
+Annotations = Globals = Locals = Mapping[str, Any]
+
+
+def eval_if_necessary(source: Any, globals: Globals, locals: Locals) -> Any:
+    if not isinstance(source, str):
+        return source
+
+    return eval(source, globals, locals)
+
+
+def get_annotations(obj: Callable[..., Any]) -> Tuple[Annotations, Globals, Locals]:
+    # Copied from https://github.com/python/cpython/blob/3.12/Lib/inspect.py#L176-L288
+	
+    if isinstance(obj, type):
+        obj_dict = getattr(obj, '__dict__', None)
+        
+        if obj_dict and hasattr(obj_dict, 'get'):
+            ann = obj_dict.get('__annotations__', None)
+            if isinstance(ann, GetSetDescriptorType):
+                ann = None
+        else:
+            ann = None
+
+        obj_globals = None
+        module_name = getattr(obj, '__module__', None)
+        
+        if module_name:
+            module = sys.modules.get(module_name, None)
+            
+            if module:
+                obj_globals = getattr(module, '__dict__', None)
+        
+        obj_locals = dict(vars(obj))
+        unwrap = obj
+    
+    elif isinstance(obj, ModuleType):
+        ann = getattr(obj, '__annotations__', None)
+        obj_globals = getattr(obj, '__dict__')
+        obj_locals = None
+        unwrap = None
+    
+    elif callable(obj):
+        ann = getattr(obj, '__annotations__', None)
+        obj_globals = getattr(obj, '__globals__', None)
+        obj_locals = None
+        unwrap = obj
+    
+    else:
+        raise TypeError(f"{obj!r} is not a module, class, or callable.")
+
+    if ann is None:
+        return {}
+
+    if not isinstance(ann, dict):
+        raise ValueError(f"{obj!r}.__annotations__ is neither a dict nor None")
+
+    if not ann:
+        return {}
+
+    if unwrap is not None:
+        while True:
+            if hasattr(unwrap, '__wrapped__'):
+                unwrap = unwrap.__wrapped__
+                continue
+            if isinstance(unwrap, functools.partial):
+                unwrap = unwrap.func
+                continue
+            break
+        if hasattr(unwrap, "__globals__"):
+            obj_globals = unwrap.__globals__
+
+    return_value = {
+        key: eval_if_necessary(value, obj_globals, obj_locals)
+        for key, value in ann.items()
+    }
+    
+    return return_value, obj_globals, obj_locals
